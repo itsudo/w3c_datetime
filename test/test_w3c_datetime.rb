89cfa7c746ee019e0dc16c266efaf65bd4cd4c60
Michal Pawlowski <misza222@gmail.com>
2013-09-03 15:41:51 +0100

    Refactor tests to use MiniTest::Spec
    As minimal required ruby is 1.9.2 there is no point to keep using
    Test::Unit

--- test/test_w3c_datetime.rb
+++ test/test_w3c_datetime.rb
@@ -1,25 +1,33 @@
-require 'test/unit'
+require 'minitest/spec'
+require 'minitest/autorun'
 require 'w3c_datetime'
 
-class W3cDatetimeTest < Test::Unit::TestCase
-  def test_parse_date
-    assert_equal W3cDatetime::parse('2013-02-10'), Time.new(2013,02,10)
+describe W3cDatetime do
+  it "parses date" do
+    W3cDatetime::parse('2013-02-10').must_equal Time.new(2013,02,10)
   end
 
-  def test_parse_date_with_hour_and_minute
-    assert_equal W3cDatetime::parse('2013-02-10T10:10'), Time.new(2013,02,10,10,10)
+  it "parses date with hour and minute" do
+    W3cDatetime::parse('2013-02-10T10:10').must_equal Time.new(2013,02,10,10,10)
   end
 
-  def test_parse_date_with_time
-    assert_equal W3cDatetime::parse('2013-02-10T10:10:20'), Time.new(2013,02,10,10,10,20)
+  it "parses date with time" do
+    W3cDatetime::parse('2013-02-10T10:10:20').must_equal Time.new(2013,02,10,10,10,20)
   end
 
-  def test_parse_date_with_time_and_milisecond
-    assert_equal W3cDatetime::parse('2013-02-10T10:10:20.30'), Time.new(2013,02,10,10,10,20)
+  it "parses date with time and miliseconds" do
+    W3cDatetime::parse('2013-02-10T10:10:20.30').must_equal Time.new(2013,02,10,10,10,20)
   end
 
-  def test_parse_date_with_hour_and_minute_and_timezone_as_offset
-    assert_equal W3cDatetime::parse('2013-02-10T10:10+10:30'),  Time.new(2013,02,10,10,10,nil,'+10:30')
+  it "parses date with hour and minute and offset" do
+    W3cDatetime::parse('2013-02-10T10:10+10:30').must_equal Time.new(2013,02,10,10,10,nil,'+10:30')
   end
 
+  it "parses date with time and offset" do
+    W3cDatetime::parse('2013-02-10T10:10:10+10:30').must_equal Time.new(2013,02,10,10,10,10,'+10:30')
+  end
+
+  it "parses date with time and miliseconds and offset" do
+    W3cDatetime::parse('2013-02-10T10:10:10.30+10:30').must_equal Time.new(2013,02,10,10,10,10,'+10:30')
+  end
 end
